69d039becf00d42ce3b0e2d219cee626417514c8
Priyanka <priyankah3730@gmail.com>
2026-09-15 15:30:12 +0530

    "Formal Output"

--- pp4.py
+++ pp4.py
@@ -1,4 +1,5 @@
 name = input("Enter your name:")
 age =  int(input("Enter your age:"))
 print(" Hello " , name , " you are " , age , " years old ")
-print(f" Hello {name} you are {age} years old ")   
+print(f" Hello {name} you are {age} years old ")
+print(f"namaste {name}")   
