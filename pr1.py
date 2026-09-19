ed200990cd2670ca4d2df4fc1324570f4c48f5e1
Priyanka <priyankah3730@gmail.com>
2026-09-19 16:53:22 +0530

    "Largest of two numbers"

---
+++ pr1.py
@@ -0,0 +1,6 @@
+num1 = int(input("Enter your 1st number: "))
+num2 = int(input("Enter your 2nd number: "))
+if num1>num2 :
+    print("number", num1 , "is larger number")
+else:
+    print("number" , num2 , "is larger number")
