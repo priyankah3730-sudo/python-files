b1a93410bc7f9f8c6817d7c59898f2646d9e331f
Priyanka <priyankah3730@gmail.com>
2026-09-19 18:14:17 +0530

    "temperature conversion from celsius to fehrnheit,vice.versa"

---
+++ pr5.py
@@ -0,0 +1,7 @@
+celsius = float(input("Enter temperature in celsius: "))
+fahrenheit = (celsius*9/5)+32
+print(f"temperature in {fahrenheit}")
+
+f = float(input("Enter temperature in fahrenheit: "))
+c = (f-32)*5/9
+print(f"temperature in {c}")
