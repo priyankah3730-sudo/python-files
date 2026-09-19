43819c147ac8ef9e5840d7a5609f3fdd4a0f84f6
Priyanka <priyankah3730@gmail.com>
2026-09-19 18:01:49 +0530

    "Average of 3 numbers"

---
+++ pr4.py
@@ -0,0 +1,7 @@
+a = int(input("Enter the value of a: "))
+b = int(input("Enter the value of b: "))
+c = int(input("Enter the value of c: "))
+avg  = (a+b+c)/3
+avg1 = (a+b+c)//3 #Floor division
+print(avg)
+print(avg1)
