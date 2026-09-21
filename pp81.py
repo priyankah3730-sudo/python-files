be7188efb6135abd28a51bcff57c8b0a4755cbf5
Priyanka <priyankah3730@gmail.com>
2026-09-21 19:22:10 +0530

    "The number is pos,neg or zero using if"

---
+++ pp81.py
@@ -0,0 +1,9 @@
+x = int(input("Enter your number: "))
+
+if x>0:
+    print(f"The number  {x} is positive number.")
+elif x<0:
+    print(f"The number {x} is negative number.")
+else:
+    print(f"The x is {x} .")
+    
