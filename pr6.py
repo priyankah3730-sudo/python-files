ee8aa83df74b5f2cd3b8d0208b79640bb3fe3a92
Priyanka <priyankah3730@gmail.com>
2026-09-21 20:24:25 +0530

    Adition of 3 numbers

---
+++ pr6.py
@@ -0,0 +1,5 @@
+a1 = int(input("Enter 1st number: "))
+a2 = int(input("Enter 2nd number: "))
+a3 = int(input("Enter 3rd number: "))
+result = (a1+a2+a3)
+print("The Sum of three numbers is>>>",result)
