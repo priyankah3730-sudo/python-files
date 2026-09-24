c5f4a0059d1f352c4d80c47f0c81abd86365c707
Priyanka <priyankah3730@gmail.com>
2026-09-24 06:19:01 +0530

    while 2

---
+++ wp2.py
@@ -0,0 +1,16 @@
+#printing number from 1 to n
+n1 = int(input("Choose one number: "))
+i = 1
+while i<=n1:
+    print(i)
+    i=i+1
+    
+    
+#printing sum of n numbers
+n2 = int(input("Enter one number: "))
+j = 1
+sum = 0
+while j<=n2:
+    sum+=j 
+    j+=1
+print(f"sum of {n2} numbers is : {sum} ")
