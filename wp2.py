8e432eebca0481648a780412c2e6f9f93b934fd3
Priyanka <priyankah3730@gmail.com>
2026-09-24 18:47:02 +0530

    while 2

--- wp2.py
+++ wp2.py
@@ -14,3 +14,14 @@ while j<=n2:
     sum+=j 
     j+=1
 print(f"sum of {n2} numbers is : {sum} ")
+
+
+#printing multiplication table
+n3 = int(input("Enter the number:"))
+print("MMultiplication table of number ",n3)
+k=1 
+
+while k<=10:
+    print(f"{n3}X{k}={n3*k}")
+    k=k+1
+    
