1cc84c1e87df2e09e409108ea62b72a2c228ea54
Priyanka <priyankah3730@gmail.com>
2026-09-24 05:55:52 +0530

    while 1

---
+++ wp1.py
@@ -0,0 +1,35 @@
+#numbers from 1 to 10
+print("Numbers>>")
+num=1
+while num<=10:
+    print(num)
+    num+=1
+    
+    
+#numbers from 10 to 1
+print("Numbers>>")
+count = 10
+while count>=1:
+    print(count)
+    count=count-1
+    
+
+#printing even numbers
+print("Even numbers>>")
+i=2
+while i<=20:
+    print(i)
+    i+=2
+    
+
+#printing odd numbers
+print("Odd numbers: ")
+j=1 
+while j<20:
+    print(j)
+    j+=2
+    
+    
+
+
+    
